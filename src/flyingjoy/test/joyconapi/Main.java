24a3317b73c7c86a673b459777b95be01ee7af9f
renardn <renardn@WEMFA4502.student.fr.ch>
2017-12-22 15:36:11 +0100

    changed main method updated lib

--- src/flyingjoy/test/joyconapi/Main.java
+++ src/flyingjoy/test/joyconapi/Main.java
@@ -21,19 +21,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Joycon joycon = new Joycon(); 
+        Joycon joycon = new Joycon(JoyconConstant.JOYCON_LEFT);
         joycon.setListener(new JoyconListener() {
             @Override
             public void handleNewInput(JoyconEvent je) {
                 for (Map.Entry<String, Boolean> entry : je.getNewInputs().entrySet()) {
-                    System.out.print("Button: "+entry.getKey()+" is "+(entry.getValue()?"ON \t":"OFF\t"));
+                    System.out.print("Button: " + entry.getKey() + " is " + (entry.getValue() ? "ON \t" : "OFF\t"));
                     if (entry.getKey().equals(JoyconConstant.CAPTURE) || entry.getKey().equals(JoyconConstant.HOME)) {
                         System.exit(0);
+                    } else if (entry.getKey().equals(JoyconConstant.MINUS) || entry.getKey().equals(JoyconConstant.PLUS)) {
+                        joycon.close();
                     }
                 }
-                System.out.println("Joystick: "+je.getJoystick());
+                System.out.println("Joystick: " + je.getJoystick());
             }
         });
     }
-    
 }
